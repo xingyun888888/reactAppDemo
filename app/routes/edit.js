ea6cb1f4ca16ec2d50b1397593cb61b14ccc828d
rubin <rubincalm@gmail.com>
2017-05-20 17:15:11 +0800

    init
    Signed-off-by: rubin <rubincalm@gmail.com>

--- app/routes/edit.js
+++ app/routes/edit.js
@@ -1,14 +1,5 @@
 module.exports = {
   path: '/edit',
-  // getChildRoutes(partialNextState, cb) {
-  //   require.ensure([], (require) => {
-  //     cb(null, [
-  //       require('./routes/Announcements'),
-  //       require('./routes/Assignments'),
-  //       require('./routes/Grades')
-  //     ])
-  //   })
-  // },
   getComponent(nextState, cb){
     require.ensure([], (require) => {
       cb(null, require('../components/edit').default)
